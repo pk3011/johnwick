fb94a48896035c025ea114c946008dd062ee4d62
Jack <92579700+JohnWickKeanue@users.noreply.github.com>
2023-01-27 22:21:59 +0530

    Update update.py

--- update.py
+++ update.py
@@ -33,13 +33,13 @@ DATABASE_URL = environ.get('DATABASE_URL', '')
 if len(DATABASE_URL) == 0:
     DATABASE_URL = None
 
-if DATABASE_URL is not None:
+"""if DATABASE_URL is not None:
     conn = MongoClient(DATABASE_URL)
     db = conn.mltb
     if config_dict := db.settings.config.find_one({'_id': bot_id}):  #retrun config dict (all env vars)
         environ['UPSTREAM_REPO'] = config_dict['UPSTREAM_REPO']
         environ['UPSTREAM_BRANCH'] = config_dict['UPSTREAM_BRANCH']
-    conn.close()
+    conn.close()"""
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
